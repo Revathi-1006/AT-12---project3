66386f719ab6c7cdb034778f70b8f308af7d41b3
Revathi <revathi8593@gmail.com>
2023-05-07 12:19:25 +0530

    project3 v1

---
+++ Test_Data/data.py
@@ -0,0 +1,80 @@
+class Revathi_Data:
+    A = "OrangeHRM"
+    B = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    C = "Admin"
+    D = "admin123"
+    #testcase1
+    m1 = "admin"
+    m2 = "pim"
+    m3 = "leave"
+    m4 = "time"
+    m5 = "recruitment"
+    m6 = "my info"
+    m7 = "performance"
+    m8 = "dashboard"
+    m9 = "directory"
+    m10 = "maintenance"
+    m11 = "buzz"
+    #test3
+    m12 = "Mohanraj"
+    m13 = "Rameshbabu"
+    m14 = "selvam"
+    m15 = "Riashree@9517"
+    m16 = "Riashree@9517"
+    #test4
+    m17 = "0266"
+    m017 = "Mohanraj"
+    #test5
+    m18 = "Reva"
+    m19 = "12345"
+    m20 = "ABCD 13254ABCD"
+    m21 = "2022-06-10"
+    m22 = "1166662"
+    m23 = "2565269"
+    m24 = "2010-07-15"
+    m25 = "No"
+    m26 = "10001"
+    #test2
+    m28 = "select" #user role
+    m29 = "select" #status
+    #test6
+    m30 = "gandhi nagar, 5th street"
+    m31 = "chennai"
+    m32 = "Tamilnadu"
+    m33 = "600008"
+    m34 = "9840456785"
+    m35 = "abcd@gmail.com"
+    #test7
+    m36 ="Mythili"
+    m37 ="Mother"
+    #test8
+    m38 = "Ria"
+    m39 = "child"
+    #test9
+    m40 = "2020-04-05"
+    m41 = "Account Assistant"
+    m42 = "Officials and Manager"
+    m43 = "Texas R&D"
+    m44 = "Full-Time Contract"
+    m45 = "2019-08-02"
+    m46 = "2020-09-05"
+    #test10
+    m47 = "2021-11-08"
+    m48 = "Deceased"
+    m49 = "good"
+    #test12
+    m50 = "45000"
+    m51 = "Grade 2"
+    m52 = "Monthly"
+    m53 = "United States Dollar"
+    m54 = "46000"
+    m55 = "9876543210"
+    m56 = "Savings"
+    m57 = "1234567"
+    m58 = "46000"
+    
+    
+
+
+
+    
---
+++ Test_Locators/locators.py
@@ -0,0 +1,105 @@
+class Revathi_Locators:
+    username_input_box = "username"  # Name
+    password_input_box = "password"  # Name
+    submit_button = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'  # XPath
+    #test1
+    search_button = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/div/div/input'  # XPath
+    #test3
+    Pim_button = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'   #pim button
+    R1 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button'  #add button
+    R2 = "firstName"
+    R3 = "lastName"
+    R4 ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[2]/div/label/span' #Create login Details
+    R5 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[3]/div/div[1]/div/div[2]/input' #username
+    R6 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[4]/div/div[1]/div/div[2]/input' #password
+    R7 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[4]/div/div[2]/div/div[2]/input' #confirm password
+    R8 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]' #save button
+    #test4
+    R9 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[1]/a' #personal details
+    R10 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]' #save button 
+    R13 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/a' #contact details
+    R14 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/a'  #emergency 
+    R15 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[4]/a' #dependants
+    R16 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[5]/a' #immigration
+    R17 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[6]/a' #job
+    R18 ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[7]/a' #salary
+    R19 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[8]/a' #Tax
+    R20 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[9]/a' #report
+    R21 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[10]/a' #qualifi
+    R22 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[11]/a' #member
+    #test5
+    R23 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[2]/div/div/div[2]/input' #NICKNAME
+    R24 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[1]/div/div[2]/input' #Employeeid
+    R25 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input' # driving licencenum
+    R26 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/div/input' # licence expiry
+    R27 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[1]/div/div[2]/input' #ssn num
+    R28 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[2]/div/div[2]/input' #sin num
+    R29 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/div/input' #DOB
+    R30 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div/div[1]/div/div[2]/input' #military service
+    R31 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[2]/div/div[2]/input' #other id
+    R32 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button' #save button
+    #test2
+    R33 = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a' #admin menu
+    R34 = '//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span' #user management
+    R35 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/div[2]/i' #user role arrow
+    R36 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[4]/div/div[2]/div/div/div[2]/i' #status arrow
+
+    #test6
+    R37 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[1]/div/div[2]/input' #street1
+    R38 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[3]/div/div[2]/input' #city
+    R39 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[4]/div/div[2]/input' #state
+    R40 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[5]/div/div[2]/input' #postal code
+    R41 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div/div[2]/div/div[2]/input' #telephone
+    R42 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div/div[1]/div/div[2]/input' #email
+    R43 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/button' #save button
+    #test7
+    R44 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/div/button' #add
+    R45 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[1]/div/div[2]/input' #name
+    R46 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/input' #relationship
+    R47 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/button[2]' #save button
+    #test8
+    R48 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/div/button' #add
+    R49 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[1]/div/div[2]/input' #name
+    R50 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/div/div/div[2]/i'#reletion arrow
+    R51 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]' #child'
+    R52 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/button[2]' #save button
+    #test9
+    R53 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[1]/div/div[2]/div/div/input' #DOJ
+    R54 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/div/div/div[2]/i' #job titleArrow
+    R55 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]' #job title
+    R56 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[4]/div/div[2]/div/div/div[2]/i' #job category arrow
+    R57 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[4]/div/div[2]/div/div/div[1]' #job category
+    R58 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[6]/div/div[2]/div/div/div[2]/i' #location arrow
+    R59 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[6]/div/div[2]/div/div/div[1]' #location
+    R60 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[7]/div/div[2]/div/div/div[2]/i' #employment arrow
+    R61 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[7]/div/div[2]/div/div/div[1]' #Employement
+    R62 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div/label/span' #include contract details 
+    R63 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div/div[1]/div/div[2]/div/div/input' #start date
+    R64 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div/div[2]/div/div[2]/div/div/input'#end date 
+    R65 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button' #save button
+    #test10
+    R66 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/button' #termination
+    R67 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div/form/div[1]/div/div[2]/div/div/input' #termination date
+    R68 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div/form/div[2]/div/div[2]/div/div/div[2]/i' #reason arrow
+    R69 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div/form/div[2]/div/div[2]/div/div/div[1]' #reason
+    R70 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div/form/div[3]/div/div[2]/textarea' #note
+    R71 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div/form/div[4]/button[2]'#save 
+    #test11
+    R72 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/button' #Activation 
+    #test12
+    R73 ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/div/button' #add button
+    R74 ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[1]/div/div[2]/input'#salary comp
+    R75 ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/div/div/div[2]/i' #pay grade arrow
+    R76 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]' #pay grade
+    R77 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[3]/div/div[2]/div/div/div[2]/i' #pay frequency arrw
+    R78 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[3]/div/div[2]/div/div/div[1]' #pay freq
+    R79 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[4]/div/div[2]/div/div/div[2]/i' #currency arrow
+    R80 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[4]/div/div[2]/div/div/div[1]' #currency
+    R81 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[5]/div/div[2]/input' #amount
+    R82 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div/label/span' #direct deposit details
+    R83 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div[1]/div[1]/div/div[2]/input' #account number
+    R84 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div[1]/div[2]/div/div[2]/div/div/div[2]/i'#account type arrow
+    R85 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div[1]/div[2]/div/div[2]/div/div/div[1]' #account type
+    R86 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div[2]/div[1]/div/div[2]/input' #routing no
+    R87 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div[2]/div[2]/div/div[2]/input' #amount
+    R88 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button[2]' #save button
